28d4c62aebfe208acd96ff8b3eb7764a9c640ae5
thirdmadman <thirdmadman@gmail.com>
2023-07-07 17:52:00 +0200

    feat: impl use of wsGlobalEventHandler

--- src/WSServer.ts
+++ src/WSServer.ts
@@ -1,10 +1,15 @@
+/* eslint-disable max-classes-per-file */
 import { WebSocketServer, WebSocket } from 'ws';
 import { ConnectionHandler } from './ConnectionHandler';
 
 import { BACKEND_HOST, BACKEND_PORT } from './common/config';
+import { WSGlobalEventHandler } from './WSGlobalEventHandler';
+import { CommunicationDTOTypes } from './dto/CommunicationDTO';
 
 export class WSServer {
   private server: WebSocketServer;
+
+  private wsGlobalEventHandler = new WSGlobalEventHandler();
 
   constructor() {
     this.server = new WebSocketServer({ port: Number(BACKEND_PORT) });
@@ -12,6 +17,12 @@ export class WSServer {
 
     const wsConnectionHandler = (ws: WebSocket) => {
       const connectionHandler = new ConnectionHandler(ws, this.server);
+      this.wsGlobalEventHandler.addConnection(connectionHandler);
+      const handler = (playerId: number, messageType: CommunicationDTOTypes, message: string) => {
+        const res = this.wsGlobalEventHandler.sentToClientWithPlayerId(playerId, messageType, message);
+        return res;
+      };
+      connectionHandler.onSendMessageToPlayerWithId = handler;
 
       ws.on('message', (data: Buffer) => connectionHandler.handleMessage(data));
     };
