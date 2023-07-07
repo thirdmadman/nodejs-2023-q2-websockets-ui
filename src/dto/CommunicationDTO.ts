158751f9687847ee557d6fe06e5c530d57c2dfd3
thirdmadman <thirdmadman@gmail.com>
2023-07-07 15:01:15 +0200

    feat: add type 'add_user_to_room'

--- src/dto/CommunicationDTO.ts
+++ src/dto/CommunicationDTO.ts
@@ -1,5 +1,16 @@
-export const AllCommunicationDTOTypes = ['reg', 'create_room', 'create_game', 'start_game', 'turn', 'attack', 'finish', 'update_room', 'update_winners'] as const;
-export type CommunicationDTOTypes =  (typeof AllCommunicationDTOTypes)[number];
+export const AllCommunicationDTOTypes = [
+  'reg',
+  'create_room',
+  'add_user_to_room',
+  'create_game',
+  'start_game',
+  'turn',
+  'attack',
+  'finish',
+  'update_room',
+  'update_winners',
+] as const;
+export type CommunicationDTOTypes = (typeof AllCommunicationDTOTypes)[number];
 export interface CommunicationDTO {
   type: CommunicationDTOTypes;
   data: string;
