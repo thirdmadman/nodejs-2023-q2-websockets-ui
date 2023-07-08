416a36fbb18336786f3325bb5b72d9651773e089
thirdmadman <thirdmadman@gmail.com>
2023-07-08 10:23:38 +0200

    feat: add GameField find methods

--- src/repository/GameFieldRepository.ts
+++ src/repository/GameFieldRepository.ts
@@ -1,5 +1,37 @@
-import { IGameField } from './../interfaces/IGameField';
+import { IGameField } from '../interfaces/IGameField';
 import { GenericRepository } from './GenericRepository';
 
-class GameFieldRepository extends GenericRepository<IGameField> {}
+class GameFieldRepository extends GenericRepository<IGameField> {
+  findAllGameFieldByGameId(gameId: number) {
+    const gameFieldResult = this.findAll();
+
+    if (!gameFieldResult || gameFieldResult.length === 0) {
+      return null;
+    }
+
+    const foundGameField = gameFieldResult.filter((gameField) => gameField.gameId === gameId);
+
+    if (!foundGameField || foundGameField.length === 0) {
+      return null;
+    }
+
+    return foundGameField;
+  }
+
+  findGameFieldByGameIdAndPlayerId(gameId: number, playerId: number) {
+    const gameFieldResult = this.findAllGameFieldByGameId(gameId);
+
+    if (!gameFieldResult || gameFieldResult.length === 0) {
+      return null;
+    }
+
+    const foundGameField = gameFieldResult.find((gameField) => gameField.playerId === playerId);
+
+    if (!foundGameField) {
+      return null;
+    }
+
+    return { ...foundGameField } as IGameField;
+  }
+}
 export const gameFieldRepository = new GameFieldRepository('gameField');
