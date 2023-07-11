ee366e1003cfa3486682a350490a22dc15f585fa
thirdmadman <thirdmadman@gmail.com>
2023-07-11 22:48:14 +0200

    feat: add findAllOwnRoomsByPlayerId

--- src/repository/RoomRepository.ts
+++ src/repository/RoomRepository.ts
@@ -1,5 +1,21 @@
 import { IRoom } from '../interfaces/IRoom';
 import { GenericRepository } from './GenericRepository';
 
-class RoomRepository extends GenericRepository<IRoom> {}
+class RoomRepository extends GenericRepository<IRoom> {
+  findAllOwnRoomsByPlayerId(playerId: number) {
+    const allRooms = this.findAll();
+
+    if (!allRooms || allRooms.length === 0) {
+      return null;
+    }
+
+    const playerOwnRooms = allRooms.filter((room) => room.playersId[0] === playerId);
+
+    if (!playerOwnRooms || playerOwnRooms.length === 0) {
+      return null;
+    }
+
+    return playerOwnRooms;
+  }
+}
 export const roomRepository = new RoomRepository('room');
